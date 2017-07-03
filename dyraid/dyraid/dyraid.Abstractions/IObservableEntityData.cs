a88cbdf1cf795d6387cc2e1648b7726dd8bea544
emekaokoro00 <emeka.okoro.00@gmail.com>
2017-07-03 01:54:12 -0500

    layout

---
+++ dyraid/dyraid/dyraid.Abstractions/IObservableEntityData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+
+namespace dyraid.Abstractions
+{
+    /// <summary>
+    /// A type that mirrors the properties of Microsoft.Azure.Mobile.Server.EntityData.
+    /// </summary>
+    public interface IObservableEntityData
+    {
+        string Id { get; set; }
+
+        DateTimeOffset CreatedAt { get; set; }
+
+        DateTimeOffset UpdatedAt { get; set; }
+
+        byte[] Version { get; set; }
+    }
+}
